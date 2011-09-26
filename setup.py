945d737f6a5b3d52f4f7eb7838b9979c6fe4469a
Ryan Petrello <lists@ryanpetrello.com>
2011-09-26 02:31:08 -0400

    Metadata updates.

--- setup.py
+++ setup.py
@@ -18,10 +18,28 @@ setup(
     * Saves compiled CSS files to a destination path, using the same directory
       structure as the source.
     """,
-    author = 'Ryan Petrello',
-    author_email = 'ryan (at) ryanpetrello.com',
-    install_requires = [],
-    test_suite='lesspy.tests.suite',
-    zip_safe = False,
-    packages = find_packages(exclude=['ez_setup'])
+    long_description     = None,
+    classifiers          = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Pre-processors',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Internet :: WWW/HTTP :: Browsers'
+    ],
+    keywords            = 'less lesscss css compile',
+    url                 = 'http://github.com/ryanpetrello/lesspy',
+    author              = 'Ryan Petrello',
+    author_email        = 'ryan (at) ryanpetrello.com',
+    license             = 'MIT',
+    install_requires    = [],
+    test_suite          = 'lesspy.tests.suite',
+    zip_safe            = False,
+    packages            = find_packages(exclude=['ez_setup'])
 )
